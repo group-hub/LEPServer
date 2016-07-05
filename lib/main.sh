4db3ca07a92b6ddf05c731fa214ae5dc5f3e04cb
John Wheal <john@whealmedia.com>
2016-07-05 12:28:24 +0100

    Increased max execution time

--- lib/main.sh
+++ lib/main.sh
@@ -32,6 +32,7 @@ function install_php_drivers {
 
 function configure_php() {
   sudo sed -i -e "s/;cgi.fix_pathinfo=1/cgi.fix_pathinfo=0/g" /etc/php5/fpm/php.ini
+  sudo sed -i -e "s/;max_execution_time = 30/max_execution_time = 240/g" /etc/php5/fpm/php.ini
   sudo service php5-fpm restart
   sudo php5enmod mcrypt
   sudo service php5-fpm restart
